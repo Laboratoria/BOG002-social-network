1728992c2efc4bdcc5e9e30b957ae07c161abbff
AngelicaT <76055451+AngelicaT319@users.noreply.github.com>
2021-04-14 08:42:08 -0500

    carpeta aplicativos

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 // Este es el punto de entrada de tu aplicacion
-import {crearHeader} from './componentes/header.js'
-import { CrearFormulario} from './componentes/formulario.js'
-import {registrar, registroGoogle} from './lib/app.js'
+import { crearHeader } from "./componentes/header.js";
+import { CrearFormulario } from "./componentes/formulario.js";
+import { registrar, registroGoogle } from "./aplicativos/app.js";
 
-const main = document.getElementById('main');
-main.appendChild( CrearFormulario());
+const main = document.getElementById("main");
+main.appendChild(CrearFormulario());
 
 registrar();
 registroGoogle();
