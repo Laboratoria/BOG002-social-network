c3ef6856b38a78d83627fc941bd4a6e040b6fa33
NataliaG9806 <garavitobarretonatalia@gmail.com>
2021-04-16 17:45:19 -0500

    mensajes de error

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,77 @@
-// aqui exportaras las funciones que necesites
-/*
-export const ImageRegister = () => {
+const firebaseConfig = {
+  apiKey: 'AIzaSyCNJzDnQucoug5jUE-5pEodaacilcQTyAw',
+  authDomain: 'nomadas-3c02d.firebaseapp.com',
+  databaseURL: 'https://nomadas-3c02d.firebaseio.com',
+  projectId: 'nomadas-3c02d',
+  storageBucket: 'nomadas-3c02d.appspot.com',
+  messagingSenderId: '189718883297',
+  appId: '1:189718883297:web:c200b22cbcb8c28c4a80a0',
 };
-*/
+
+firebase.initializeApp(firebaseConfig);
+
+export const Information = (username, email, password) => {
+  function messageError(errorCode) {
+    const popupEmail = document.querySelector('.popupEmail');
+    const popupPassword = document.querySelector('.popupPassword');
+    switch (errorCode) {
+      case 'auth/email-already-in-use':
+        popupEmail.style.display = 'block';
+        popupEmail.innerHTML = 'Este correo ha sido registrado por otro usuario';
+        // popupEmail.classList.toggle('show');
+        setTimeout(() => {
+          popupEmail.style.display = 'none';
+        }, 5000);
+        break;
+      case 'auth/invalid-email':
+        popupEmail.style.display = 'block';
+        popupEmail.innerHTML = 'Correo invalido';
+        // popupEmail.classList.toggle('show');
+        setTimeout(() => {
+          popupEmail.style.display = 'none';
+        }, 5000);
+        break;
+      case 'auth/weak-password':
+        popupPassword.style.display = 'block';
+        popupPassword.innerHTML = 'Contraseña invalida';
+        // popupPassword.classList.toggle('show');
+        setTimeout(() => {
+          popupPassword.style.display = 'none';
+        }, 3000);
+        break;
+      default:
+        break;
+    }
+  }
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+  })
+    .catch((error) => {
+      const errorCode = error.code;
+      messageError(errorCode);
+    });
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // Updates the user attributes:
+      user.updateProfile({
+        // <-- Update Method here
+        displayName: username,
+        // photoURL: 'https://example.com/jane-q-user/profile.jpg'
+      })
+        .then(
+          () => {
+            // Profile updated successfully!
+            const displayName = user.displayName;
+            // var photoURL = user.photoURL;
+          },
+          (error) => {
+            const errorCode = error.code;
+            messageError(errorCode);
+          },
+        );
+    }
+  });
+};
