fdfc25a65ee55e1b6705fe09618358fb4a0dd9a0
Hohjanna <hohjanna@MacBook-Pro-de-Hohjanna.local>
2021-04-19 17:53:04 -0500

    Changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
+import Home from './lib/Home.js';
+import Register from './lib/Register.js';
+import Login from './lib/Login.js';
 
 myFunction();
