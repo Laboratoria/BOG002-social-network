b61c64a014d675967729610bc2e9f0c6c5d171fe
Hohjanna <hohjanna@MacBook-Pro-de-Hohjanna.local>
2021-04-19 18:28:56 -0500

    Adding pages.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
+import home from './lib/home.js';
+import register from './lib/register.js';
+import login from './lib/login.js';
 
 myFunction();
