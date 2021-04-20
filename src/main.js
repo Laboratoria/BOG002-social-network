6af82c6eed47f01c3f71f4b96fced755135c0b0b
Natalia Gonzalez <nataliasita04@gmail.com>
2021-04-19 20:26:51 -0500

    inicio de test interfaz registro

--- src/main.js
+++ src/main.js
@@ -3,13 +3,66 @@
 import { Information } from './lib/index.js';
 
 // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+let arrayUser;
 
 function userRegistration(event) {
   event.preventDefault();
-  const username = document.querySelector('.inputNameRegister').value;
+  // const username = document.querySelector('.inputNameRegister').value;
   const email = document.querySelector('.inputEmailRegister').value;
   const password = document.querySelector('.inputPasswordRegister').value;
-  Information(username, email, password);
+
+  Information(email, password).then((user) => {
+    // console.log(user);
+    arrayUser = user;
+    const usuario = document.createElement('div');
+    usuario.innerHTML = arrayUser;
+    usuario.className = 'divGhost';
+    //  assignName(username);
+    const divFormRegister = document.getElementById('divFormRegister');
+    const confirmationRegistro = document.getElementById('formRegister');
+    confirmationRegistro.style.display = 'none';
+    const messageExito = document.createElement('div');
+    messageExito.textContent = 'Tu registro se ha realizado con exito!!!!';
+    messageExito.className = 'messageExitoso';
+    divFormRegister.appendChild(messageExito);
+    const SignupRegister = document.createElement('button');
+    SignupRegister.type = 'button';
+    SignupRegister.innerText = 'Iniciar sesion';
+    SignupRegister.className = 'buttonInicioSesion';
+    messageExito.appendChild(SignupRegister);
+  }).catch((error) => {
+    // console.log(error);
+    const popupEmail = document.querySelector('.popupEmail');
+    const popupPassword = document.querySelector('.popupPassword');
+    switch (error) {
+      case 'auth/email-already-in-use':
+        popupEmail.style.display = 'block';
+        popupEmail.innerHTML = 'Su correo ya se encuentra registrado';
+        // popupEmail.classList.toggle('show');
+        setTimeout(() => {
+          popupEmail.style.display = 'none';
+        }, 5000);
+        break;
+      case 'auth/invalid-email':
+        popupEmail.style.display = 'block';
+        popupEmail.innerHTML = 'Correo invalido';
+        // popupEmail.classList.toggle('show');
+        setTimeout(() => {
+          popupEmail.style.display = 'none';
+        }, 5000);
+        break;
+      case 'auth/weak-password':
+        popupPassword.style.display = 'block';
+        popupPassword.innerHTML = 'Contraseña invalida';
+        // popupPassword.classList.toggle('show');
+        setTimeout(() => {
+          popupPassword.style.display = 'none';
+        }, 3000);
+        break;
+      default:
+        break;
+    }
+  });
 }
 
 document.querySelector('.formRegister').addEventListener('submit', userRegistration);
