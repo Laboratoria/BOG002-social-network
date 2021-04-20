32cd165e5810b2ae58c458b8d74f0a0b6604e1ed
ginnaaceror <ginnapaolaacerorussi@MacBook-Pro-de-Ginna.local>
2021-04-20 10:48:43 -0500

    Mauqetación inicio de sesión

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-// Este es el punto de entrada de tu aplicacion
-
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+const btnA = document.querySelector('btnSing-in');
+const divB = document.getElementById('Login');
+const divC = document.getElementById('New-account');
+
+btnA.addEventListener('click', () => {
+    if(divB.style.display === 'none'){
+        divC.style.display = 'block';
+    } else {
+        divB.style.display = 'none';
+    }
+});
