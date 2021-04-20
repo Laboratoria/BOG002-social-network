9f30769b3cd40031246ab3378e4c7dafa28720b1
carfolacam98 <laura.carfolacam@gmail.com>
2021-04-20 13:24:33 -0500

    no funciona el case aiuda

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,24 @@
 // aqui exportaras las funciones que necesites
 
+
+
 export const myFunction = () => {
   // aqui tu codigo
-  console.log('Hola mundo!');
+
+  window.addEventListener('hashchange',()=>{
+    let hashito=window.addEventListener('hashchange')
+    router(hashito);
+    console.log(hashito)
+  });
+};
+
+export const router = (route)=>{
+  switch (route){
+      case '/#inicio':
+          return console.log ("aiuda")
+     case '/#registro':
+         return console.log("esta funcionando registro ")
+        //  default:
+        //      return console.log("si funciona pero  esta enlazando la principal")
+  }
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
-
+import { router} from './lib/index.js';
 myFunction();
+router();
+
