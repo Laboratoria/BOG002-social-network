5c7a7ed6f4d9dd0bd64b9de580499203a8068bc4
AngelicaT <76055451+AngelicaT319@users.noreply.github.com>
2021-04-21 08:57:44 -0500

    añadida nueva hoja de estilos

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
   },
   rules: {
     "import/extensions":0,
+    "quotes":0,
   },
 };
--- src/componentes/formulario.js
+++ src/componentes/formulario.js
@@ -2,13 +2,17 @@ export default function CrearFormulario() {
   const formulario = document.createElement('form');
 
   const html = `
-    <div><input type="email" class="inputIngreso" id="usuario" placeholder="Ingresar correo" required/></div>
-            <div><input type="password" class="inputIngreso" id="contrasena" placeholder="Ingresar contraseña" 
-            required minlength="6"/></div>
+    <div>
+    <p class="teachers-font">Si ya tienes cuenta, inicia sesión!</p>
+    <input type="email" class="inputIngreso" id="usuario" placeholder="Ingresar correo" required/>
+            <input type="password" class="inputIngreso" id="contrasena" placeholder="Ingresar contraseña" 
+            required minlength="6"/>
             <div id="mesagge"></div>
-            <div><a href="#registro">Nuevo registro</a> <button id="btnI">Ingresar</button></div>
+             <button id="btnI">Iniciar Sesion</button>
+             <p id="registro">¿No tienes cuenta? Registrate <a href="#registro">AQUÍ</a>
+             <p id="registro"">------------------  O  ------------------</p>
             <img src="img/singFacebook.png" alt="Registro con Facebook" id="botonFacebook"/>
-            <div><img src="img/th.jpg" alt="Registro con Google" id="botonGoogle"/></div>
+            <img src="img/th.jpg" alt="Registro con Google" id="botonGoogle"/></div>
     
     `;
   formulario.innerHTML = html;
