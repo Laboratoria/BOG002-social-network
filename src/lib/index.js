1a801684c3d151799b0d5a9a1ee77d672bb9ee9d
carfolacam98 <laura.carfolacam@gmail.com>
2021-04-20 22:57:35 -0500

    por fin funciona

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,24 +1,37 @@
 // aqui exportaras las funciones que necesites
 
 
+const idregistro= document.getElementById("registro")
+
+
+const vistas = ()=>{
+  const texto=
+  `<h5>Holis esto es una prueba</h5>
+  <p>A ver si esta verga funciona</p>`;
+  const divregistro=document.createElement('div');
+  divregistro.innerHTML=texto;
+  console.log(vistas)
+  return divregistro
+  };
+const router = (route)=>{
+  console.log(route)
+  switch (route){
+    case '#/inicio':
+        return console.log ("aiuda")
+   case '#/registro':{
+     return idregistro.appendChild(vistas())
+   }
+       default:
+           return console.log("si funciona pero  esta enlazando la principal")
+      
+}
+};
 
 export const myFunction = () => {
   // aqui tu codigo
 
   window.addEventListener('hashchange',()=>{
-    let hashito=window.addEventListener('hashchange')
-    router(hashito);
-    console.log(hashito)
+    router(window.location.hash);
+    
   });
-};
-
-export const router = (route)=>{
-  switch (route){
-      case '/#inicio':
-          return console.log ("aiuda")
-     case '/#registro':
-         return console.log("esta funcionando registro ")
-        //  default:
-        //      return console.log("si funciona pero  esta enlazando la principal")
-  }
 };
