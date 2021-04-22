eded6c078512888c4109e9110646d3e0ca707f5e
hohjanna <ardilayoki@gmail.com>
2021-04-22 11:03:10 -0500

    Estructuración de archivos

--- src/main.js
+++
@@ -1,8 +0,0 @@
-// Este es el punto de entrada de tu aplicacion
-
-import { myFunction } from './lib/index.js';
-import Home from './lib/Home.js';
-import Register from './lib/Register.js';
-import Login from './lib/Login.js';
-
-myFunction();
