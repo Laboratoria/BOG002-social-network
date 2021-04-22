402d4c7ffe303484f63704f3fa6737a3b72c8c61
carfolacam98 <laura.carfolacam@gmail.com>
2021-04-22 17:18:02 -0500

    ya funciona registro

--- src/lib/vistas.js
+++ src/lib/vistas.js
@@ -1,13 +1,6 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable indent */
-export const registro = () => {
-    const texto = `<h5>Holis esto es una prueba</h5>
-    <p>A ver si esto funciona</p>`;
-    const divregistro = document.createElement('div');
-    divregistro.innerHTML = texto;
-    console.log(divregistro);
-    return divregistro;
-    };
+
 
 export const formularioRegistro = () => {
     class formRegistro extends HTMLElement {
