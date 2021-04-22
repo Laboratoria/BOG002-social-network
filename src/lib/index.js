c54314a66527b9e90862fee9830c9c89e99649b6
nicol <nicolmadero12@gmaicom>
2021-04-22 11:07:00 -0500

    web component de los primeros botones

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,26 +1,24 @@
 // aqui exportaras las funciones que necesites
-import {registro} from './vistas.js';
+import { registro } from './vistas.js';
 
-let idregistro= document.getElementById("registro")
+const idregistro = document.getElementById('registro');
 
-const router = (route)=>{
-  idregistro.innerHTML="";
-  console.log(route) 
-  switch (route){
+const router = (route) => {
+  idregistro.innerHTML = '';
+  console.log(route);
+  switch (route) {
     case '#/inicio':
-        return console.log ("aiuda")
-   case '#/registro':{
-     return idregistro.appendChild(registro())
-   }
-       default:
-           return console.log("si funciona pero  esta enlazando la principal")
-      
-}
+      return console.log('aiuda');
+    case '#/registro': {
+      return idregistro.appendChild(registro());
+    }
+    default:
+      return console.log('si funciona pero  esta enlazando la principal');
+  }
 };
 
 export const myFunction = () => {
-  window.addEventListener('hashchange',()=>{
+  window.addEventListener('hashchange', () => {
     router(window.location.hash);
-    
   });
 };
