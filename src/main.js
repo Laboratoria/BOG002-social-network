ccfc4d9ee042dedd86bc881e7e8740a2cabc6248
carfolacam98 <laura.carfolacam@gmail.com>
2021-04-25 17:23:20 -0500

    adelantado

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 /* eslint-disable import/named */
 // Este es el punto de entrada de tu aplicacion
 import { router} from './lib/enrutador.js';
+//import { formularioRegistro} from './lib/vistas.js';
 
 const init = () => {
     //pintar la vista en la que estoy
     window.addEventListener('hashchange', () => {
-        router(window.location.hash);
+      var componente=document.getElementById("opcionesInicio");
+        router(window.location.hash,componente);
       });
 }
 init();
+
+
+//formularioRegistro();
+
+
+
 
 
 
