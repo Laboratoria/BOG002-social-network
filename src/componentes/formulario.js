f70cfb4c8d3431c92d3f6c8e282cfa50dfff2d70
AngelicaT <76055451+AngelicaT319@users.noreply.github.com>
2021-04-26 13:06:53 -0500

    cambios a estilos

--- src/componentes/formulario.js
+++ src/componentes/formulario.js
@@ -11,8 +11,8 @@ export default function CrearFormulario() {
              <div id="mesagge"></div>
              <p id="registro">¿No tienes cuenta? Registrate <a href="#registro">AQUÍ</a>
              <p id="registro">------------------  O  ------------------</p>
-            <span class="login facebook"><img src="img/facebookLogo2.png" alt="Registro con Facebook" id="botonFacebook" class="icono"/><p class="parrafos">Iniciar Sesión con Facebook</p></span>
-            <span class="login"><img src="img/googleLogo.png" alt="Registro con Google" id="botonGoogle" class="icono"/><p class="parrafos">Iniciar Sesión con Google</p></span>
+            <span class="login facebook" id="botonFacebook"><img src="img/facebookLogo2.png" alt="Registro con Facebook"  class="icono"/><p id="parrafos">Iniciar Sesión con Facebook</p></span>
+            <span class="login" id="botonGoogle"><img src="img/googleLogo.png" alt="Registro con Google" class="icono"/><p id="parrafos">Iniciar Sesión con Google</p></span>
             </div>
     
     `;
