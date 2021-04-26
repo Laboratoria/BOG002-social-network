e778a222c71e98462f9e0ad0e82925ecd3d2d572
carfolacam98 <laura.carfolacam@gmail.com>
2021-04-26 12:49:30 -0500

    funciona auth y tiene lo de grid

--- src/lib/registrar.js
+++ src/lib/registrar.js
@@ -4,7 +4,8 @@ export function registro(){
  let botonregistro= document.querySelector('.registerbtn');
  var email=document.getElementById("email");
 var password=document.getElementById("psw");
-botonregistro.addEventListener('click', ()=>{firebase.auth().createUserWithEmailAndPassword(email, password)
+botonregistro.addEventListener('click', ()=>{firebase.auth().createUserWithEmailAndPassword("nicolmadero12@gmail.com"
+    , "password")
     .then((userCredential) => {
       // Signed in
       var user = userCredential.user;
