ff25fbdad0b4f5b989a67cfffded178785806172
carfolacam98 <laura.carfolacam@gmail.com>
2021-04-26 22:10:11 -0500

    ya funciona tambien autentificacion

--- src/lib/registrar.js
+++ src/lib/registrar.js
@@ -2,23 +2,25 @@
 
 export function registro(){
  let botonregistro= document.querySelector('.registerbtn');
-botonregistro.addEventListener('click', ()=>{console.log("diste click")});
-var email=document.getElementById("email");
-var password=document.getElementById("psw");
 
-firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    console.log(errorCode)
-    var errorMessage = error.message;
-    console.log(errorMessage)
-    // ..
-  });
+
+botonregistro.addEventListener('click', ()=>{ 
+    var email=document.getElementById("email").value;
+    var password=document.getElementById("psw").value;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      console.log(errorCode)
+      var errorMessage = error.message;
+      console.log(errorMessage)
+      // ..
+    });});
+
 }
 
 
