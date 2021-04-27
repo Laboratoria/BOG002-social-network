6b8a748c79584dfa3abb5023f33eacb8b254823d
JessiJoya <jessica.joyaq@gmail.com>
2021-04-26 20:05:35 -0500

    creamos los elementos del inciio y les dimos un poco estilo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-// Este es el punto de entrada de tu aplicacion
+// importamos las funciones que crear los elementos y las llamamos con los addeventlistener asi interactuamos con el DOM
 
-import { myFunction } from './lib/index.js';
-
-myFunction();
+import { pantallaInicio} from './lib/index.js';
+let rootHtml = document.getElementById("root");
+// rootHtml.addEventListener("onload", cargarPantalla())
+window.onload=cargarPantalla;
+console.log(typeof cargarPantalla)
+function cargarPantalla (){
+rootHtml.appendChild(pantallaInicio());
+}
