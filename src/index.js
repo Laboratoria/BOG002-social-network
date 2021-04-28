aa755f6feeb0e73009fc88b0d202aca5b479a2bc
saragutierrez15 <saragutierrez31@gmail.com>
2021-04-28 13:24:50 -0500

    Funcion cerrar sesion terminada

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+//Function Register
 export const register = (email, password) => {
     return firebase.auth().createUserWithEmailAndPassword(email, password);
 };
-    
+
+//Función Login
 export const Login = (email, password) => {
     return firebase.auth().signInWithEmailAndPassword(email, password);
 };
+
+//Función Register Google
+export const registerGoogle = (provider) => {
+    return firebase.auth().signInWithPopup(provider);
+};
+
+//Función Sing Out
+export const signOut = () => {
+    return firebase.auth().signOut();
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+//
 import { router } from './router/router.js';
 
 window.addEventListener('load', () => {    
@@ -11,64 +12,5 @@ window.addEventListener('hashchange', () => {
 })
 
 
-
-
-// // Este es el punto de entrada de tu aplicacion
-
-
-// //Hide the 2nd screen
-// document.getElementById("screenLogIn").style = "display:normal"
-// document.getElementById("screenRegistration").style = "display:none"
-// document.getElementById("screenWall").style = "display:none"
-
-
-// //Button (crear Cuenta)
-// const btnCreateAccount = document.getElementById("btnCreateAccount");
-// btnCreateAccount.addEventListener('click', () => {
-//     document.getElementById("screenLogIn").style = "display:none"
-//     document.getElementById("screenRegistration").style = "display:normal"
-// });
-
-// //Button (cerrar Registro)
-// const btnCloseRegistration = document.getElementById("btnCloseRegistration");
-// btnCloseRegistration.addEventListener('click', () => {
-//     document.getElementById("screenRegistration").style = "display:none"
-//     document.getElementById("screenLogIn").style = "display:normal"
-// });
-
-// // Button (iniciar sesión)
-// const btnSignIn = document.getElementById("btnLogIn");
-// btnSignIn.addEventListener('click', () => {
-//     let email = document.getElementById('signInEmail').value
-//     let password = document.getElementById('signInpassword').value
-//     if (email && password) {
-//         //if (email != "" && password != "")
-//         //if (email.length>0 && password.length>0)
-
-//         login(email, password)
-//             .then(() => {
-//                 document.getElementById("screenRegistration").style = "display:none"
-//                 document.getElementById("screenLogIn").style = "display:none"
-//                 document.getElementById("screenWall").style = "display:normal"
-//             })
-//             .catch(() => {
-//                 console.log("ocurrio un error")
-//             })
-//     }
-//     else {
-//         console.log("error ingresar email")
-//     }
-
-//})
-
-
-
-// Button (Cerrar Sesión)
-// const btnLogOut = document.getElementById("btnLogOut");
-// btnLogOut.addEventListener('click', () => {
-//     document.getElementById("screenRegistration").style = "display:none"
-//     document.getElementById("screenLogIn").style = "display:normal"
-//     document.getElementById("screenWall").style = "display:none"
-// });
 
 
