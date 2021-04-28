fa3bdca30cf7ca567e821c4650026a7415c61c37
Leidy <paoalm1@hotmail.com>
2021-04-28 11:18:15 -0500

    Funcion boton registrar

--- src/lib/SingUp.js
+++ src/lib/SingUp.js
@@ -1,4 +1,21 @@
 // creacion de formulario sign up
 
-const formularioSignUp = document.getElementsByTagName("main");
-formularioSignUp.innerHTML 
+export  function FormularioDeRegistro(){
+    const NewForm = document.createElement("form");
+    console.log(NewForm)
+    let SignUp = document.getElementsByTagName("main")
+    Formulario = `
+    <form>
+    <h1> Created account </h1>
+    
+    <input type="text" placeholder="User name"> 
+    <input type="email" placeholder="email"> 
+    <input type="password" placeholder="password> 
+    
+    <button type="submit" id="Register" class="btn" > <a href="#Register"> REGISTER</a></button>
+    
+    </form>`
+    NewForm.appendChild(Formulario)
+    SignUp.innerHTML = NewForm
+    
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
-
+import {FormularioDeRegistro} from './lib/SingUp.js';
 myFunction();
+
+
+const submit = document.querySelector("#signUp");
+submit.addEventListener("click", MostrarRegistro)
+
+function MostrarRegistro(){
+    
+    const prueba = FormularioDeRegistro
+    // console.log(prueba)
+    return prueba
+ }
+
+ 
