59334d1fd5f5ae5191daaa91bb985ed9d65f789c
Astrid Piñero <astrid.pinero85@gmail.com>
2021-04-28 22:11:40 -0500

    nada

--- src/main.js
+++
@@ -1,9 +0,0 @@
-// importamos las funciones que crear los elementos y las llamamos con los addeventlistener asi interactuamos con el DOM
-import { pantallaInicio} from './lib/index.js';
-let rootHtml = document.getElementById("root");
-// rootHtml.addEventListener("onload", cargarPantalla())
-window.onload=cargarPantalla;
-console.log(typeof cargarPantalla)
-function cargarPantalla (){
-rootHtml.appendChild(pantallaInicio());
-}
