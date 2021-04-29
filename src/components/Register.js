2cf0a72146cb5d78b137816e986e1bceb2546b07
hohjanna <ardilayoki@gmail.com>
2021-04-29 08:53:59 -0500

    Console log

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,9 +2,9 @@ import { reset } from "./Utils.js";
 //import { google_provider } from '../index.js';
 
 export function Register() {
-  reset();
-  const register = document.createElement("div");
-  register.innerHTML = ` 
+    reset();
+    const register = document.createElement("div");
+    register.innerHTML = ` 
         <div class="container">
           <a href="#/"><img class="back" src=./assets/back.svg alt="arrow"></a>
           <h1 class="register-tittle"> Enjoy Gleam </h1>
@@ -42,81 +42,74 @@ export function Register() {
               </span>
             </div>`;
 
-  return register;
+    return register;
 }
 
 export function addUser() {
-  const auth = firebase.auth();
-  // const firestore = firebase.firestore(); --> TODO FIRESTORE
-  const btnContinue = document.querySelector("#register");
-  const formRegister = document.querySelector("#formRegister");
-  const btnFb = document.querySelector("#logoFb");
-  const btnGoogle = document.querySelector("#logoGoogle");
-  btnContinue.addEventListener("click", (e) => {
-    e.preventDefault();
+    const auth = firebase.auth();
+    // const firestore = firebase.firestore(); --> TODO FIRESTORE
+    const btnContinue = document.querySelector("#register");
+    const formRegister = document.querySelector("#formRegister");
+    const btnFb = document.querySelector("#logoFb");
+    const btnGoogle = document.querySelector("#logoGoogle");
+    btnContinue.addEventListener("click", (e) => {
+        e.preventDefault();
 
-    const email = document.querySelector("#email").value;
-    const password = document.querySelector("#password").value;
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        formRegister.reset();
-        console.log("estas registrado");
-      });
-  });
+        const email = document.querySelector("#email").value;
+        const password = document.querySelector("#password").value;
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                formRegister.reset();
+                console.log("estas registrado");
+            });
+    });
 
-  btnGoogle.addEventListener("click", (e) => {
-    const google_provider = new firebase.auth.GoogleAuthProvider();
-    auth
-      .signInWithPopup(google_provider)
-      .then((result) => {
-        /** @type {firebase.auth.OAuthCredential} */
-        let credential = result.credential;
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        let token = credential.accessToken;
-        // The signed-in user info.
-        let user = result.user;
-        // ...
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        let errorCode = error.code;
-        let errorMessage = error.message;
-        // The email of the user's account used.
-        let email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        let credential = error.credential;
-        // ...
-      });
-  });
+    btnGoogle.addEventListener("click", (e) => {
+        const google_provider = new firebase.auth.GoogleAuthProvider();
+        auth
+            .signInWithPopup(google_provider)
+            .then((result) => {
+                /** @type {firebase.auth.OAuthCredential} */
+                let credential = result.credential;
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                let token = credential.accessToken;
+                // The signed-in user info.
+                let user = result.user;
+                // ...
+            })
+            .catch((error) => {
+                console.log("error 3312");
+            });
+    });
 
-  btnFb.addEventListener("click", (e) => {
-    const providerFb = new firebase.auth.FacebookAuthProvider();
-    firebase
-      .auth()
-      .signInWithPopup(providerFb)
-      .then((result) => {
-        /** @type {firebase.auth.OAuthCredential} */
-        let credential = result.credential;
+    btnFb.addEventListener("click", (e) => {
+        const providerFb = new firebase.auth.FacebookAuthProvider();
+        firebase
+            .auth()
+            .signInWithPopup(providerFb)
+            .then((result) => {
+                /** @type {firebase.auth.OAuthCredential} */
+                let credential = result.credential;
 
-        // The signed-in user info.
-        let user = result.user;
+                // The signed-in user info.
+                let user = result.user;
 
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-        let accessToken = credential.accessToken;
+                // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+                let accessToken = credential.accessToken;
 
-        // ...
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        let errorCode = error.code;
-        let errorMessage = error.message;
-        // The email of the user's account used.
-        let email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        let credential = error.credential;
+                // ...
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                // The email of the user's account used.
+                let email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                let credential = error.credential;
 
-        // ...
-      });
-  });
+                // ...
+            });
+    });
 }
