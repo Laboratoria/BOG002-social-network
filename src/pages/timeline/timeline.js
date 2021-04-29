25573135ee1c7b69c47a73f3145c7e6b0c756fde
saragutierrez15 <saragutierrez31@gmail.com>
2021-04-29 12:16:09 -0500

    post desde firebase

--- src/pages/timeline/timeline.js
+++ src/pages/timeline/timeline.js
@@ -1,7 +1,7 @@
 import { signOut } from '../../index.js';
 
 
- export function timelinePage() {
+export function timelinePage() {
 
     const view = `
         <section id="screenWall">
@@ -16,11 +16,17 @@ import { signOut } from '../../index.js';
                 <form id="logOut">
                     <button id="btnLogOut">Cerrar sesion</button>
                 </form>
+                <div class = "container" id = "containerPosts">
+                    <div id ="posts">
+                        <ul id = "listPost">
+                        </ul>
+                    </div>
+                </div>
             </main>
         </section>    
     `
     const divElement = document.createElement('div');
-    divElement.innerHTML = view;   
+    divElement.innerHTML = view;
 
     return divElement;
 }
@@ -31,8 +37,8 @@ export function signOutGoogle() {
     buttonSignOut.addEventListener('click', function () {
         signOut()
             .then(() => {
-                console.log ('cerraste sesión')
-                window.location = '#/'                
+                console.log('cerraste sesión')
+                window.location = '#/'
             })
             .catch(err => {
                 console.log(err)
@@ -40,5 +46,38 @@ export function signOutGoogle() {
 
     });
 }
+export function postsTimeline() {
+    const postlist = document.getElementById('listPost');
+    const setPost = data => {
+        if (data.length) {
+            let html = '';
+            data.forEach(doc => {
+                const post = doc.data();
+                const li = `
+            <li>
+            <h5>${post.titulo}</h5>
+            <p>${post.contenido}</p>
+            </li>
+            `;
+                html += li;
 
-//export default timelinePage;
+            });
+            postlist.innerHTML = html;
+        } else {
+            postlist.innerHTML = '<p>No hay publicaciones</p>'
+        }
+    }
+
+    // Eventos 
+    auth.onAuthStateChanged(user => {
+        if (user) {
+            fireStore.collection('posts')
+                .get()
+                .then((snapshot) => {
+                    setPost(snapshot.docs)
+                })
+        } else {
+            console.log('cerraste sesion')
+        }
+    })
+}
