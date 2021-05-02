a29e184d4883a94a20198a6b538c15a0ff454aaf
Leidy <paoalm1@hotmail.com>
2021-05-02 16:53:55 -0500

    captura de valores y condicionales de campos vacios

--- src/lib/SingUp.js
+++ src/lib/SingUp.js
@@ -12,14 +12,53 @@ export function FormularioDeRegistro(){
     NewForm.innerHTML= `
     
     <h1> Created account </h1>
-    
-    <input type="text" class="input" placeholder="User name"> 
-    <input type="email" class="input" placeholder="email"> 
-    <input type="password" class="input" placeholder ="password"> 
+    <fieldset>
+
+    <input type="text" id="NameUser" class="input"  placeholder="User name" maxlength="10"   title="Maximo 16 caracteres" required
+    "> 
+    <span id="CampoVacioName"></span>
+
+    <input type="email"id="EmailUser"class="input" placeholder="email"> 
+    <span id="CampoVacioEmail"></span>
+
+    <input type="password" id="PasswordUser"class="input" placeholder ="password"> 
+    <span id="CampoVacioPassword"></span>
     
     <button type="submit" id="Register" class="btn" > REGISTER <a href="#Register"> </a> </button>
-    
+    </fieldset>
     `;
    document.getElementById("Body").appendChild(NewForm)
+
+// Valores de los inputs
+
+
+
+   const Submit    = document.getElementById("Register")    
+         Submit.addEventListener("click", (event)=>{
+
+         event.preventDefault();
+         console.log("enviado")
+         
+   const Name     = document.getElementById("NameUser").value;
+   const Email    = document.getElementById("EmailUser").value;
+   const Password = document.getElementById("PasswordUser").value; 
+
+      if (Email == "") {
+         document.getElementById("CampoVacioEmail").textContent = "Este campo esta vacio"
+         
+      }
+       else if (Name == "") {
+         document.getElementById("CampoVacioName").textContent = "Este campo esta vacio"
+         
+      }
+       else if (Password =="") {
+         document.getElementById("CampoVacioPassword").textContent = "Este campo esta vacio"
+         
+      }
+ 
+})
   
 }
+
+  
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
-import {FormularioDeRegistro} from './lib/SingUp.js';
+import {FormularioDeRegistro } from './lib/SingUp.js';
 import {FormularioDeIngreso} from './lib/Login.js';
   
 myFunction();
@@ -11,12 +11,10 @@ myFunction();
 
 
 
-
-
 // Mostrando pantallas de registro e inicio
 
 const Registro = document.querySelector("#signUp");
-      Registro .addEventListener("click", MostrarRegistro)
+      Registro.addEventListener("click", MostrarRegistro)
 
 const ingresoUsuario = document.querySelector("#login");
       ingresoUsuario .addEventListener("click", MostrarIngreso);
