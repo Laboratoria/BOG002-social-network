03063e8ac7001344bfc588e16407c09facc055da
leysanchezb92 <leysanchezb92@hotmail.com>
2021-05-03 11:36:22 -0500

    first test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 // eslint-disable-next-line import/no-cycle
-import { App } from "./app/App.js";
+// import { App } from "./app/App.js";
+import { Router } from "./components/Router.js";
 
-//export const google_provider = new firebase.auth.GoogleAuthProvider()
+export function App() {
+  Router();
+}
 
 document.addEventListener("DOMContentLoaded", App);
 window.addEventListener("hashchange", App);
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCYY8zyVFV_RB_SDWHdE2WVybpxlM35AkU",
-    authDomain: "sn-gleam.firebaseapp.com",
-    projectId: "sn-gleam",
-    storageBucket: "sn-gleam.appspot.com",
-    messagingSenderId: "454363590320",
-    appId: "1:454363590320:web:0809dc8ad3570efc371575"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCYY8zyVFV_RB_SDWHdE2WVybpxlM35AkU",
+  authDomain: "sn-gleam.firebaseapp.com",
+  projectId: "sn-gleam",
+  storageBucket: "sn-gleam.appspot.com",
+  messagingSenderId: "454363590320",
+  appId: "1:454363590320:web:0809dc8ad3570efc371575",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,21 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+// import { Router } from "./components/Router.js";
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+describe("Prueba de navegacion", () => {
+  beforeEach(() => {
+    global.firebase = {
+      auth: jest.fn(),
+    };
+  });
+  it("Rutas protegidas por sesion", () => {
+    global.firebase.auth.mockImplementation(() => ({
+      currentUsser: null,
+    }));
+  });
+  it("Usuario nuevo con correo y contraseña", () => {
+    global.firebase.auth.mockImplementation(() => ({
+      currentUsser: "hohjanna@hotmail.com",
+      passwordUsser: "3312123",
+    }));
   });
 });
