30c66a5ed1ec3cb4c52e1e8ef2271ac27cd62205
Ruby <alvarezruby098@gmail.com>
2021-05-12 10:47:34 -0500

    error

--- src/Firebase/conf.js
+++ src/Firebase/conf.js
@@ -1,20 +1,20 @@
-import firebase from 'firebase'
-import 'firebase/firestore'
 // Your web app's Firebase configuration
     
 var firebaseConfig = {
-    apiKey: "AIzaSyDybXyutc7WWfHJ-3cWqb0wqDmXLpECSuY",
-    authDomain: "social-network-19982.firebaseapp.com",
-    projectId: "social-network-19982",
-    storageBucket: "social-network-19982.appspot.com",
-    messagingSenderId: "728662470096",
-    appId: "1:728662470096:web:37d6354953b938300d1dd5",
-    measurementId: "G-C6G1E4E0TP"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-  console.log('firebase configurado');
+  apiKey: "AIzaSyDybXyutc7WWfHJ-3cWqb0wqDmXLpECSuY",
+  authDomain: "social-network-19982.firebaseapp.com",
+  projectId: "social-network-19982",
+  storageBucket: "social-network-19982.appspot.com",
+  messagingSenderId: "728662470096",
+  appId: "1:728662470096:web:37d6354953b938300d1dd5",
+  measurementId: "G-C6G1E4E0TP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+console.log('firebase configurado');
+
+
 
    
