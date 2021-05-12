8d05a837c65735b0879646f871945e9d7621ef32
Ruby <alvarezruby098@gmail.com>
2021-05-12 11:38:14 -0500

    estilos menu hamburguesa

--- src/Pages/inicio.js
+++ src/Pages/inicio.js
@@ -4,12 +4,12 @@ export function inicio(){
     
 	<div id="encabezado">
 		<div id="logo"> FoodFans </div>
-		<div id="configuracion"> Configuracion</div> 
+		<div id="configuracion"><img src="./imagenes/Setting.svg"></div> 
 	</div>
 
 	<div id="menu">
-		<span>Cambiar nombre</span>
-		<span>Cerra sesion</span>
+		<span class= "setting">Cambiar nombre</span>
+		<span class= "setting">Cerra sesion</span>
 	</div>
 
 	<div id="publicaciones">
