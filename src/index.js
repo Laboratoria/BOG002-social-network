b00de55d5887bedd6f1e0eeb340f26483fba4d74
leysanchezb92 <leysanchezb92@hotmail.com>
2021-05-13 17:53:50 -0500

    first post

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-cycle
 // import { App } from "./app/App.js";
+
 import { Router } from "./components/Router.js";
 
 export function App() {
@@ -8,14 +9,3 @@ export function App() {
 
 document.addEventListener("DOMContentLoaded", App);
 window.addEventListener("hashchange", App);
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCYY8zyVFV_RB_SDWHdE2WVybpxlM35AkU",
-  authDomain: "sn-gleam.firebaseapp.com",
-  projectId: "sn-gleam",
-  storageBucket: "sn-gleam.appspot.com",
-  messagingSenderId: "454363590320",
-  appId: "1:454363590320:web:0809dc8ad3570efc371575",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
