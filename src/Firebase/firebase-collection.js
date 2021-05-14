ac2640754586d10f4dc36b33d433d1ff7b5e9e6a
Ruby <alvarezruby098@gmail.com>
2021-05-14 11:53:39 -0500

    archivo perfil.js y creacion de perfil.css,hay que arreglarlo :(

--- src/Firebase/firebase-collection.js
+++ src/Firebase/firebase-collection.js
@@ -11,3 +11,17 @@ db.collection("Publicaciones").add({
 .catch((error) => {
     console.error("Error adding document: ", error);
 });
+
+export const SaveUser = (user) => {
+    db.collection("usuarios").add({
+       user
+    })
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
+    
+    
+  }
--- src/Pages/perfil.js
+++ src/Pages/perfil.js
@@ -1,34 +1,62 @@
+import {SaveUser} from '../Firebase/firebase-collection.js'
+
+
+
 export function perfil(){
 
     let html= `
-    
-    
-    
-	<div id="encabezado">
-		<div id="logo"> FoodFans </div>
-		<div id="configuracion"><img src="./imagenes/Setting.svg"></div> 
-	</div>
-
-	<div id="menu">
-		<span class= "setting">Cambiar nombre</span>
-		<span class= "setting" id="cerrar-sesion">Cerra sesion</span>
-	</div>
-
-	<div id="profile">
-		 Pepito Perez
-	</div>
-
-	<nav>
+	
+	<div class="container">
+	 <nav>
+	   <div class="menu">
+		<div class="logo"> 
+		<h1>FoodFans<h1></div>
+		<div id="configuracion"><img src="./imagenes/Setting.svg">
+		<div class="enlaces" id="enlaces">
+          <a href="" id="editar-perfil">Editar Perfil</a>
+		  <span class= "setting" id="cerrar-sesion">Cerra sesion</span>
+		</div>
+		</div> 
+		
+	  </nav>
+      <section>
+	  <form>
+	  <input type="text" class="input" id="name" placeholder="Name" name="name"></input>
+	  <input type="text" class="input"id="apellido" placeholder="Apellido" name="apellido"></input>
+	  <input type="text" class="input" id="descripcion" placeholder="Descripcion" name="descripcion"></input>
+	  <button id='guardar'>Guardar</button>
+      </form>
+	  
+	  </section>
+	
+	  <footer>
 		<ul>
 		<li><a href="#/release"><img src="./imagenes/Home.svg"></a>Inicio </li>
 		<li><a href="#/profile"><img src="./imagenes/Profile.svg"></a>Perfil</li>
-		<li><a href="#/search"><img src="./imagenes/Search.svg"></a>Buscar</li>
-
-		</ul>
-	</nav>
-
-`
-        
-        
+		<li><a href="#/search"><img src="./imagenes/Search.svg"></a>Buscar </li>
+       </ul>
+	  </footer>
+	</div>`;    
     return html;
   }
+  
+  export function EditarPerfil (){
+    const guardarCambios = document.getElementById('guardar');
+	guardarCambios.addEventListener('click',(event) =>{
+		event.preventDefault();
+       let nombre = document.getElementById('name').value;
+	   let apellido = document.getElementById('apellido').value;
+	   let descripcion = document.getElementById('descripcion').value;
+		console.log('hola')
+
+		const user = {
+			nombre,
+			apellido,
+			descripcion
+		}
+		SaveUser(user)
+	});
+
+
+	}
+	
