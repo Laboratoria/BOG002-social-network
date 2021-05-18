7bae9b46abb828de9458d1f54489819103b7449c
hohjanna <ardilayoki@gmail.com>
2021-05-17 23:28:14 -0500

    Create post done

--- src/Firebase/Storage.js
+++ src/Firebase/Storage.js
@@ -1,28 +1,29 @@
+import { renderPost } from "../components/Utils.js";
+
 const db = firebase.firestore();
 export function createPost(file, description) {
-  uploadImage(file).then((url) => {
-    console.log(url);
-    db.collection("usersPost").doc().set({
-      image: url,
-      description,
+    uploadImage(file).then((url) => {
+        db.collection("usersPost").doc().set({
+            image: url,
+            description,
+        });
     });
-  });
-}
-function uploadImage(file, img) {
-  const name = file.name;
-  const ref = firebase.storage().ref("/userProfileImgs/" + name);
-  const uploadTask = ref.put(file);
+    db.collection("usersPost").onSnapshot(query => {
+        let changePost = query.docChanges();
+        changePost.forEach(post => {
+            renderPost(post.doc.data());
+        })
+    });
 
-  //   function () {
-  //     uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-  //       firebase.db.ref("usersPost/");
-  //     });
-  //   }
-  // );
-  // return snapshot.ref.getDownloadURL();
-  return uploadTask
-    .then((snapshot) => snapshot.ref.getDownloadURL())
-    .catch((error) => {
-      console.log(error.message);
-    });
+}
+
+function uploadImage(file) {
+    const name = file.name;
+    const ref = firebase.storage().ref("/userProfileImgs/" + name);
+    const uploadTask = ref.put(file);
+    return uploadTask
+        .then((snapshot) => snapshot.ref.getDownloadURL())
+        .catch((error) => {
+            console.log(error.message);
+        });
 }
--- src/components/Router.js
+++ src/components/Router.js
@@ -3,43 +3,44 @@ import { Landing, random } from "./Landing.js";
 import { Register, addUser } from "./Register.js";
 import { Home, menu } from "./Home.js";
 import { Login, logInUser } from "./Login.js";
-import { Post, menuHam } from "./Post.js"
+import { Post, menuHam, postFb } from "./Post.js"
 import { Password, recoverPassword } from "./Password.js";
 
 export function Router() {
-  const { hash } = location;
-  const root = document.querySelector("#template");
-  switch (hash) {
-    case "":
-    case "#/":
-      root.appendChild(Loading());
-      break;
-    case "#/landing":
-      root.appendChild(Landing());
-      random();
-      break;
-    case "#/register":
-      root.appendChild(Register());
-      addUser();
-      break;
-    case "#/login":
-      root.appendChild(Login());
-      logInUser();
-      break;  
-    case "#/home":
-      root.appendChild(Home());
-      // userOut();
-      menu();
-      break;
-    case "#/post":
-      root.appendChild(Post());
-      menuHam();
-      break;
-    default:
-      break;
-    case "#/password":
-      root.appendChild(Password());
-      recoverPassword();
-      break;      
-  }
+    const { hash } = location;
+    const root = document.querySelector("#template");
+    switch (hash) {
+        case "":
+        case "#/":
+            root.appendChild(Loading());
+            break;
+        case "#/landing":
+            root.appendChild(Landing());
+            random();
+            break;
+        case "#/register":
+            root.appendChild(Register());
+            addUser();
+            break;
+        case "#/login":
+            root.appendChild(Login());
+            logInUser();
+            break;
+        case "#/home":
+            root.appendChild(Home());
+            // userOut();
+            menu();
+            break;
+        case "#/post":
+            root.appendChild(Post());
+            menuHam();
+            postFb();
+            break;
+        default:
+            break;
+        case "#/password":
+            root.appendChild(Password());
+            recoverPassword();
+            break;
+    }
 }
