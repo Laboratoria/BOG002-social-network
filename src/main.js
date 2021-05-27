8ab4e16ae9bbdbbc81eb1383beecc462c13a0685
angiecortesdev <angiecortes29@gmail.com>
2021-05-27 10:12:26 -0500

    se agrego código en el main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import { router } from './router/index.routes.js';
 
-myFunction();
+router(window.location.hash)
+window.addEventListener('hashchange', () => {
+    router(window.location.hash)
+})
