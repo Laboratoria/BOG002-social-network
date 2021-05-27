c8e6f832b8f5f2c0282c3e00129b4598b978a20a
brisney quiñonez <brisneyducuara@gmail.com>
2021-05-27 09:52:03 -0500

    se agrego el nuevo archivo router

---
+++ index.routes.js
@@ -0,0 +1,22 @@
+import Home from "../views/home.js"
+import Login from "../views/formLogin.js"
+
+let content = document.getElementById("root");
+
+const router = (route) => {
+  content.innerHTML = '';
+  switch (route) {
+    case "#/Home": {
+      return content.appendChild(Home());
+    }
+
+    case "#/login": {
+      return content.appendChild(Login());
+    }
+
+    default:
+      return console.log("Error 404");
+  }
+};
+
+export { router };
