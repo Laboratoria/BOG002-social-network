87d2d2855bfc1578f1dac9b1f48e85ff8a59324a
Rosanny Oropeza <rosanny.oropeza82@gmail.com>
2021-05-26 23:38:16 -0500

    Control de likes y dislikes segun usuario actual

--- src/pages/timeline/timeline.js
+++ src/pages/timeline/timeline.js
@@ -106,41 +106,38 @@ function showPosts(doc) {
   const post = doc.data();
   const idPost = post.Uid;
   const idUser = auth.currentUser.uid;
-  console.log(idPost, idUser)
-
-     let btnEditar = ``;
-     let btnDelete = ``;
-
-    if (idPost === idUser) { 
-      btnEditar = `<button type="button" class="btnEditPost" data-id="${doc.id}"><img id="imageEdit" src="assets/logoEditar.png"></button>`;
-      btnDelete = `<button type="button" class="btnDeletePost" data-id="${doc.id}"><img id="imageDelete" src="assets/logoEliminar.png"></button>`;
-    }//Mostrar botones editar y eliminar publicacion del mismo usuario
-    const li = `
-      <li id="${doc.id}" >
-        <div id="containerTitlePost">
-          <img id="imgUserMobile" src="assets/IconoUsuario.png">
-          <h3 id="titlePost">${post.Title}</h3>
-        </div>
-        <div id="containerDate"> 
-          <p id="datePost">${(new Date(post.Date.seconds * 1000)).toLocaleDateString('es-CO')}</p>
-          ${btnEditar}
-          ${btnDelete}
-        </div>                
-        <input value='${post.Contents}' id="textPost${doc.id}" disabled = "true" ></input>
-        <div id="containerLikes">
-          <span class="likesCounter" id="likePost${doc.id}">0</span>
-          <button type="button" class="btnLikes" id="btnLikes${doc.id}" data-id="${doc.id}" title="Dar Like">
-            <img id="imageLike" src="assets/IconoCorazonLinea.png">
-          </button>
-          <button type="button" class="btnLikes" id="btnDisLikes${doc.id}" data-id="${doc.id}" title="Dar DisLike">
-            <img id="imageDisLike" src="assets/IconoCorazon2.png">
-          </button>
-        </div>       
-      </li>
-      `;
-    html += li;
-    postlist.insertAdjacentHTML('afterbegin', html);
-    getLikes(doc.id);
+  let btnEditar = ``;
+  let btnDelete = ``;
+  if (idPost === idUser) { 
+    btnEditar = `<button type="button" class="btnEditPost" data-id="${doc.id}"><img id="imageEdit" src="assets/logoEditar.png"></button>`;
+    btnDelete = `<button type="button" class="btnDeletePost" data-id="${doc.id}"><img id="imageDelete" src="assets/logoEliminar.png"></button>`;
+  }//Mostrar botones editar y eliminar publicacion del mismo usuario
+  const li = `
+    <li id="${doc.id}" >
+      <div id="containerTitlePost">
+        <img id="imgUserMobile" src="assets/IconoUsuario.png">
+        <h3 id="titlePost">${post.Title}</h3>
+      </div>
+      <div id="containerDate"> 
+        <p id="datePost">${(new Date(post.Date.seconds * 1000)).toLocaleDateString('es-CO')}</p>
+        ${btnEditar}
+        ${btnDelete}
+      </div>                
+      <input value='${post.Contents}' id="textPost${doc.id}" disabled = "true" ></input>
+      <div id="containerLikes">
+        <span class="likesCounter" id="likePost${doc.id}">0</span>
+        <button type="button" class="btnLikes" id="btnLikes${doc.id}" data-id="${doc.id}" title="Dar Like">
+          <img id="imageLike" src="assets/IconoCorazonLinea.png">
+        </button>
+        <button type="button" class="btnLikes" id="btnDisLikes${doc.id}" data-id="${doc.id}" title="Dar DisLike">
+          <img id="imageDisLike" src="assets/IconoCorazon2.png">
+        </button>
+      </div>       
+    </li>
+    `;
+  html += li;
+  postlist.insertAdjacentHTML('afterbegin', html);
+  getLikes(doc.id);
 }
 
 // Función modal para eliminar Publicaciones
@@ -220,37 +217,42 @@ function newCollectionLikes(idPost) {
     btnLikes.style.display = 'none';
     btnDisLikes.style.display = 'block';
   });
-
-
 }
 
 function getLikes(idPost) {
   const datalikes = [];
   let likes = 0;
+  let userActualLikes = false;
   const btnDisLikes = document.getElementById(`btnDisLikes${idPost}`);
   const btnLikes = document.getElementById(`btnLikes${idPost}`);
   getLikesPost(idPost)
     .then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
         datalikes.push(doc.data());
+        //si el usuario actual ya dio like, activamos variable de control
         if (doc.data().Uid === auth.currentUser.uid) {
+          userActualLikes = true;
+          //agg click del btn disLike
           btnDisLikes.addEventListener('click', () => {
             deleteLikes(doc.id);
-            btnDisLikes.style.display = 'none';
-            btnLikes.style.display = 'block';
             getLikes(idPost);
-          })
-        }        
+          });
+        }
       });
-      likes = datalikes.length;
-      if (likes === 0){
-        btnDisLikes.style.display = 'none';
-        btnLikes.style.display = 'block';
-      }else{
-        btnDisLikes.style.display = 'block';
+      //validamos si el usuario actual dio like para mostrar btn disLike
+      if (userActualLikes){
         btnLikes.style.display = 'none';
+        btnDisLikes.style.display = 'block';          
+      }else{
+        //else mostrar btn like
+        btnLikes.style.display = 'block';
+        btnDisLikes.style.display = 'none';
       }
-
+      likes = datalikes.length;
+      //si no tienes likes es porque apenas lo se esta publicando, mostar btnLike
+      if (likes === 0){
+        btnLikes.style.display = 'block';
+      }
       const likePost = document.getElementById(`likePost${idPost}`);
       likePost.innerHTML = likes;
     })
