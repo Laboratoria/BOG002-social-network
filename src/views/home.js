4834481d1bf804260f8bcb030884666626aee9f7
zagu5 <zury.agudelo@gmail.com>
2021-06-01 11:29:54 -0500

    se hacen cambios en form registro con base en lo registrado en planning

--- src/views/home.js
+++ src/views/home.js
@@ -5,14 +5,17 @@ export default () => {
     <p>What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
  <form id="formRegistro">
     <h2>Bienvenid@s a Universo Marvel</h2>
-    <h5>registrate para saber mas</h5> 
-    <input type="text" placeholder="Correo" id="correo">
+    <h2>Crear Cuenta</h2> 
+    <label for="usuario">Usuario</label>
+    <input type="text" placeholder="Ingrese usuario" id="usuario">
+    <label for="correo">Correo</label>
+    <input type="text" placeholder="Ingresa un correo válido" id="correo">
+    <label for="contrasena">Contraseña</label>
     <input type="password" placeholder="Crea una Contraseña" id="contrasena">
-    <button type="submit">Continuar</button>
+    <button type="submit">Registrar</button>
     
     <p>O</p>
-    <input type="button" value="Registrarse con Facebook">
-    <input type="button" value="Registrarse con Google">
+    <input type="button" value="registrarse con Google">
     <p>¿Ya eres miembro? <a href="">Inicia Sesión</a></p>
  </form>
  `;
--- src/views/login.js
+++ src/views/login.js
@@ -1,12 +1,13 @@
 export default () => {
     let viewsLogin = `
         <form id="formLogin">
+            <label for="correo">correo</label>
             <input type="text" value="Correo" id="correoLogin">
+            <label for="contrasena">Contraseña</label>
             <input type="password" value="Contraseña" id="contrasenaLogin">
             <button type="submit">Continuar</button>
             <p>O</p>
-            <input type="button" value="Ingresar con Facebook">
-    <input type="button" value="Ingresar con Google">
+            <input type="button" value="Ingresar con Google">
         </form>
     `
 
