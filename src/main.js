dbbd3c5d58ad4c88f378d9b04ed65fb18c8c1165
angiecortesdev <angiecortes29@gmail.com>
2021-06-02 11:44:16 -0500

    update

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { router } from './router/index.routes.js';
-import { registroUsuario} from './firebaseAuth.js';
+import { registroUsuario } from './firebaseAuth.js';
 
 router(window.location.hash);
 window.addEventListener('hashchange', () => {
-  router(window.location.hash);
+    router(window.location.hash);
 });
 
 const formRegistro = document.getElementById('formRegistro');
 // const formLogin = document.getElementById('formLogin');
 
 formRegistro.addEventListener('submit', (event) => {
-  const correo = document.getElementById('correo').value;
-  const contrasena = document.getElementById('contrasena').value;
-  event.preventDefault();
-  registroUsuario(correo.trim(), contrasena.trim());
+    const correo = document.getElementById('correo').value;
+    const contrasena = document.getElementById('contrasena').value;
+    event.preventDefault();
+    registroUsuario(correo.trim(), contrasena.trim());
 });
 
 // formLogin.addEventListener('submit', (event) => {
-//   const correoLogin = document.getElementById('correoLogin').value;
-//   const contrasenaLogin = document.getElementById('contrasenaLogin').value;
-//   event.preventDefault();
-//   loginUsuario(correoLogin.trim(), contrasenaLogin.trim());
+//     const correoLogin = document.getElementById('correoLogin').value;
+//     const contrasenaLogin = document.getElementById('contrasenaLogin').value;
+//     event.preventDefault();
+//     loginUsuario(correoLogin.trim(), contrasenaLogin.trim());
 // });
-console.log(firebase);
+// Console.log(firebase);
--- src/views/login.js
+++ src/views/login.js
@@ -8,7 +8,7 @@ export default () => {
             <input type="button" value="Ingresar con Facebook">
     <input type="button" value="Ingresar con Google">
         </form>
-    `
+    `;
 
     let elemento2 = document.createElement('div');
     elemento2.innerHTML = viewsLogin;
